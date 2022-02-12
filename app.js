06497bf909ff88e5b585890687bb39241487b574
nazmul162001 <nazmulprime162001@gmail.com>
2022-02-13 01:46:20 +0600

    update subtotal price

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
-function updateCaseNumber(product, price, isIncreasing){
-  const caseInput = document.getElementById(product + '-number');
-  let caseNumber = caseInput.value;
+function updateProductNumber(product, price, isIncreasing){
+  const productInput = document.getElementById(product + '-number');
+  let productNumber = productInput.value;
   if(isIncreasing == true){
-    caseNumber = parseInt(caseNumber) + 1;
+    productNumber = parseInt(productNumber) + 1;
   }
-  else if(caseNumber > 0) {
-    caseNumber = parseInt(caseNumber) - 1;
+  else if(productNumber > 0) {
+    productNumber = parseInt(productNumber) - 1;
   }
-  caseInput.value = caseNumber;
+  productInput.value = productNumber;
 
   // update case total 
-  const caseTotal = document.getElementById(product + '-total');
-  caseTotal.innerText = caseNumber * price;
+  const productTotal = document.getElementById(product + '-total');
+  productTotal.innerText = productNumber * price;
+
+  // calculateTotal 
+  calculateTotal();
+}
+
+// totalCalculation 
+function getInputValue(product){
+  const productInput = document.getElementById(product + '-number');
+  const productNumber = parseInt(productInput.value);
+  return productNumber;
+}
+
+function calculateTotal(){
+  const phoneTotal = getInputValue('phone') * 1219;
+  const caseTotal = getInputValue('case') * 59;
+  const subTotal = phoneTotal + caseTotal;
+  // update subtotal 
+  document.getElementById('sub-total').innerText = subTotal;
 }
 
 // Phone increase Decrease Handle
 document.getElementById('phone-plus').addEventListener('click', function(){
-  updateCaseNumber( 'phone', 1219 ,true);
+  updateProductNumber( 'phone', 1219 ,true);
 })
 document.getElementById('phone-minus').addEventListener('click', function(){
-  updateCaseNumber( 'phone', 1219 ,false);
+  updateProductNumber( 'phone', 1219 ,false);
 })
-
-
 
 
 
@@ -32,12 +48,21 @@ document.getElementById('case-plus').addEventListener('click', function(){
   // const caseInput = document.getElementById('case-number');
   // const caseNumber = caseInput.value;
   // caseInput.value = parseInt(caseNumber) + 1;
-  updateCaseNumber( 'case', 59 ,true);
+  updateProductNumber( 'case', 59 ,true);
 })
 
 document.getElementById('case-minus').addEventListener('click', function(){
   // const caseInput = document.getElementById('case-number');
   // const caseNumber = caseInput.value;
   // caseInput.value = parseInt(caseNumber) - 1;
-  updateCaseNumber( 'case', 59 ,false);
+  updateProductNumber( 'case', 59 ,false);
 })
+
+
+// // update total 
+// const phoneTotal = document.getElementById('phone-total');
+// const caseTotal = document.getElementById('case-total');
+// const totalPrice = parseFloat(phoneTotal.innerText) + parseFloat(caseTotal.innerText);
+
+// const subTotal = document.getElementById('sub-total');
+// const finalTotal = subTotal.innerText = totalPrice;
